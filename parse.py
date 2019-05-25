66dbffeeb00b6eb472dcb5d647f3574105d34aa2
eugene.roslyakov <jenecka0@gmail.com>
2019-05-25 14:07:52 +0300

    comment print join

--- parse.py
+++ parse.py
@@ -7,7 +7,7 @@ raw_text_data = sys.argv[2]
 re_table = textfsm.TextFSM(template)
 data = re_table.ParseText(raw_text_data)
 
-print(', '.join(re_table.header))
+# print(', '.join(re_table.header))
 
 for row in data:
-    print(', '.join(row))
+    print(row)
