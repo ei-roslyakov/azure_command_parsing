eb284dd2c81c1a97ed00a8ab54c211ea13446f6c
eugene <jenecka0@gmail.com>
2019-06-04 12:24:55 +0300

    error correction

--- wide_data_parsing.py
+++ wide_data_parsing.py
@@ -1,11 +1,14 @@
+import argparse
 import codecs
 import io
 import sys
 import textfsm
 from dateutil import parser
 
-AMOUNT_REMAINING = int(sys.argv[1]) * -1
+
 TEMPLATE_FILE_NAME = "./templates/wide_data.template"
+INDEX_COLUMN_TIME_CREATE = 0
+INDEX_COLUMN_NAME = 3
 
 
 def get_text_from_file(file_name):
@@ -25,7 +28,16 @@ def sort_pole(elem):
     return elem[2]
 
 
-def main():
+def parse_command_line():
+    parsers = argparse.ArgumentParser()
+    parsers.add_argument('-q',
+                         default=-5,
+                         type=int)
+
+    return parsers.parse_args()
+
+
+def parse_data():
     data_to_be_parsed = sys.stdin.read()
     template_io = create_stream_from_text_file(TEMPLATE_FILE_NAME)
 
@@ -37,10 +49,15 @@ def main():
         item[2] = parser.parse(item[2])
         new_wide_data.append(item)
 
-    new_wide_data.sort(key=sort_pole)
-    for item in new_wide_data[:AMOUNT_REMAINING]:
-        print(item[0], item[3])
+    return new_wide_data
+
+
+def main(data_to_be_sorted):
+    amount_remaining = parse_command_line()
+    data_to_be_sorted.sort(key=sort_pole)
+    for item in data_to_be_sorted[:amount_remaining.q * -1]:
+        print(item[INDEX_COLUMN_TIME_CREATE], item[INDEX_COLUMN_NAME])
 
 
 if __name__ == '__main__':
-    main()
+    main(parse_data())
